c48c3443b1b95ac4c9abdf0bde20e152a31ccb53
mindsky <romain.rondet@gmail.com>
2022-01-19 19:03:59 +0100

    add filtering features

--- backend/apps/animes/views.py
+++ backend/apps/animes/views.py
@@ -1,5 +1,6 @@
 """Animes views."""
 
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import viewsets
 from .models import Anime
 from .serializers import AnimeSerializer
@@ -8,5 +9,14 @@ from .serializers import AnimeSerializer
 class AnimeViewSet(viewsets.ReadOnlyModelViewSet):
     """Animes model viewset."""
 
-    queryset = Anime.objects.all().order_by("id")
     serializer_class = AnimeSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["english_name", "japanese_name"]
+    ordering_fields = ["start_date", "score"]
+
+    def get_queryset(self):
+        queryset = Anime.objects.all()
+        category = self.request.query_params.get("categories")
+        if category is not None:
+            queryset = queryset.filter(categories__name=category.title())
+        return queryset
