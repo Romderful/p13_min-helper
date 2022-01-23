2e317324a9760098e255dc942b16d2a56589a919
mindsky <romain.rondet@gmail.com>
2022-01-23 21:03:14 +0100

    refacto - snake_case to camelCase

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="getUserSearch" class="col-12 col-lg-auto mb-3 mb-lg-0">
     <input
-      v-model="user_input"
+      v-model="userInput"
       type="search"
       class="form-control"
       placeholder="Search..."
@@ -18,12 +18,12 @@ export default {
   name: "SearchBar",
   data() {
     return {
-      user_input: "",
+      userInput: "",
     };
   },
   methods: {
     async getUserSearch() {
-      this.$store.dispatch("updateUserInput", this.user_input);
+      this.$store.dispatch("updateUserInput", this.userInput);
       if (this.$route.name == "Animes") {
         const response = await getData(
           `api-v1/animes/?search=${this.getUserInput}`
@@ -32,7 +32,7 @@ export default {
       } else {
         this.$router.push("Animes");
       }
-      this.user_input = "";
+      this.userInput = "";
     },
   },
   computed: {
