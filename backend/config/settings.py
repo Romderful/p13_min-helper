8e09b7c6b8f1ee41ff6ea6c77deb495b8c99240f
mindsky <romain.rondet@gmail.com>
2022-03-06 19:11:28 +0100

    update deployment settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,7 +148,7 @@ if DEBUG:
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = BASE_DIR / "static"
-    STATICFILES_DIRS = str(BASE_DIR / "static")
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
