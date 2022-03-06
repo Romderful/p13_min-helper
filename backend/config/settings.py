69a8e473333932b1e7ecb8bb3aa479f708a98849
mindsky <romain.rondet@gmail.com>
2022-03-06 19:45:04 +0100

    update deployment settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,6 +150,7 @@ if DEBUG:
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
