a79f3258911da160a6cdf5b25d332fdf5a1edf41
mindsky <romain.rondet@gmail.com>
2022-03-06 18:36:33 +0100

    update deployment settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,7 +147,7 @@ if DEBUG:
 
 else:
     STATIC_URL = "/static/"
-    STATIC_ROOT = BASE_DIR / "static"
+    STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_DIRS = [VUE_DIST]
 
 # Default primary key field type
